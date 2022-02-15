0a0e1deb894e483d1de2dfd23628fec38fadf60e
kimson <67375278+i-am-webbi@users.noreply.github.com>
2022-02-15 11:01:41 +0300

    Add files via upload

---
+++ emailmcusensor/emailmcusensor.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+const char *ssid =  "webbi";     
+const char *pass =  "Kimson006";
+const int sensorPin= A0;
+int lpg;
+void send_event(const char *event);
+const char *host = "maker.ifttt.com";
+const char *privateKey = "grWdafoqpqv7Fdsnihs_H8QfUPDaI9DwBeR1uuNwfhm";
+WiFiClient client;
+void setup() 
+{
+       Serial.begin(115200); 
+       delay(10);
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+       WiFi.begin(ssid, pass); 
+       while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(550);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ }
+void loop() 
+{
+     lpg = !analogRead(sensorPin);
+     Serial.print("Gas concentration is:");
+     Serial.println(lpg);   
+     if (lpg <=30)
+     {
+     
+     send_event("gas_checker");                 
+     }
+}
+void send_event(const char *event)
+{
+  Serial.print("Connecting to ");
+  Serial.println(host);  
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Connection failed");
+    return;
+  }  
+ 
+  String url = "/trigger/";
+  url += event;
+  url += "/with/key/";
+  url += privateKey;  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);  
+
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  while(client.connected())
+  {
+    if(client.available())
+    {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    } else {
+     
+      delay(50);
+    };
+  }
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
